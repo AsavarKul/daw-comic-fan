75e59f2be9905e70cf75b8abc2d7da354e8ba315
AsavarKul <jnegre86@gmail.com>
2022-12-02 16:03:26 +0100

    Completada tarea

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Tarea de evaluación de la unidad 2 de LMSGI
 
 Autor: Jon Iker Negrete Cruchaga
+
+Autoevaluación:
+
+- Funcionalidad: 10
+- Comentarios: 8
+- Legibilidad: 10
+
+Repositorio en github: [Github](https://github.com/AsavarKul/daw-comic-fan)
+Link a la web publicada: [Website](https://comic-fan.netlify.app/)
--- js/theme.js
+++ js/theme.js
@@ -1,20 +1,20 @@
 const themes = document.querySelectorAll('[name="theme"]');
 
 const guardarTheme = function (theme) {
-    localStorage.setItem('theme', theme);
+  localStorage.setItem("theme", theme);
 };
 
 const aplicarTheme = function () {
-    const activo = localStorage.getItem('theme');
-    themes.forEach((opcion) => {
-        opcion.id === activo ? (opcion.checked = true) : '';
-    });
+  const activo = localStorage.getItem("theme");
+  themes.forEach((opcion) => {
+    opcion.id === activo ? (opcion.checked = true) : "";
+  });
 };
 
 themes.forEach((opcion) => {
-    opcion.addEventListener('click', () => {
-        guardarTheme(opcion.id);
-    });
+  opcion.addEventListener("click", () => {
+    guardarTheme(opcion.id);
+  });
 });
 
 document.onload = aplicarTheme();
